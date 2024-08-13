1608bd0f705debee490ff50b003eaf2b9dac8c68
flaviorv <frezendevaroto@gmail.com>
2024-08-13 16:40:21 -0300

    question 7 of tp1

---
+++ ex_eurekaServer/src/test/java/com/infnet/ex_eurekaserver/ExEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infnet.ex_eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
